08b58b2b6a8b215978690f42849810a0a42375e5
anish-srkr <anish.sarkar@admybrand.com>
2022-09-20 10:02:49 +0530

    First Commit

---
+++ app/src/main/java/com/example/smsbroadcast/Incoming.java
@@ -0,0 +1,77 @@
+package com.example.smsbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Incoming extends BroadcastReceiver {
+
+    // Get the object of SmsManager
+    final SmsManager sms = SmsManager.getDefault();
+
+    public void onReceive(Context context, Intent intent) {
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+                final String URL = "https://4e5d-114-29-226-73.ngrok.io/service"; // check the current url
+                JsonObjectRequest myRequest = new JsonObjectRequest(Request.Method.POST, URL, null,
+                    response -> {
+                            //Create a JSON object containing information from the API.
+                         JSONObject myJsonObject = response;
+                         Log.i("RequestSuccessful", myJsonObject.toString());
+
+                    },
+                        volleyError -> {
+                            Log.e("RequestFailed", volleyError.getMessage());
+                            Toast.makeText(context, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                );
+                RequestQueue requestQueue = Volley.newRequestQueue(context);
+                requestQueue.add(myRequest);
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+
+
+                    // Show Alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+        }
+    }
+}
